64ab55635adb8cfd0493b560417da6c583f1baad
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-03-01 06:36:25 +0300

    seeded counties

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_032958) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_033101) do
+  create_table "constituencies", force: :cascade do |t|
+    t.string "name"
+    t.integer "county_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["county_id"], name: "index_constituencies_on_county_id"
+  end
+
   create_table "counties", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -30,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_032958) do
     t.string "profile_picture"
   end
 
+  add_foreign_key "constituencies", "counties"
 end
