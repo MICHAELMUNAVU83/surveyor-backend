204117691742c0fd8ebea45a71e67b5976d0b362
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-03-02 19:04:05 +0300

    Added survey create action

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,2 +1,14 @@
 class SurveysController < ApplicationController
+    def create
+        survey = Survey.create(survey_params)
+        render json: survey
+    end
+
+    private
+
+    def survey_params
+
+        params.require(:survey).permit(:average_salary_per_month, :average_rent_per_month, :happy_with_taxes, :happy_with_MCA, :happy_with_MP, :happy_with_county_gov, :happy_with_nat_gov, :user_id)
+    end
+    
 end
